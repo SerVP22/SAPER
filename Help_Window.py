817a13ea4bab8bc4e0ffe60c17cb4011f3dea103
SerVP <s_pahomov@mail.ru>
2023-05-06 00:16:59 +0700

    Закончено окно справки

--- Help_Window.py
+++ Help_Window.py
@@ -43,7 +43,7 @@ class HelpWin:
                          text_color="black", font=("Arial", 20, ), fg_color="transparent" )
         if self.bomb_img:
             lbl1.configure(image=self.bomb_img, hover=False, compound="left", border_spacing=5)
-        lbl1.grid(row=0, column=1, columnspan=8)
+        lbl1.grid(row=0, column=0, columnspan=10)
 
         # текстовый блок 1
 
@@ -63,12 +63,12 @@ class HelpWin:
 
         if h00_img and h01_img:
             CTkLabel(frm_0, image=h00_img,text="" ).grid(row=2, column=0, columnspan=6, padx=10, pady=5)
-            CTkLabel(frm_0, image=h01_img, text="").grid(row=2, column=6, columnspan=4, padx=10, pady=5,
+            CTkLabel(frm_0, image=h01_img, text="").grid(row=2, column=6, columnspan=4, padx=15, pady=5,
                                                          sticky="e")
 
         # текстовый блок 2
 
-        tb2 = CTkTextbox(frm_0, width=420, height=190, fg_color="grey90", corner_radius=10, )
+        tb2 = CTkTextbox(frm_0, width=420, height=340, fg_color="grey90", corner_radius=10, )
         tb2.insert("0.0",
         """         Если   в   соседних  клетках  мин  нет,  то   в   таком  случае 
 клетка останется пустой.   Если у клетки в соседях есть другие 
@@ -80,51 +80,45 @@ class HelpWin:
 закончится.
          Для   успешного   прохождения  игры  SAPER  необходимо, 
 ориентируясь    на    количество    мин    в   открытых    клетках, 
-пометить флажками все заминированые клетки.""")
+пометить флажками все заминированые клетки.
+
+                                    НАСТРОЙКИ СЛОЖНОСТИ
+                                
+        Сложность    игры    устанавливается    в   меню   в    разделе 
+"Настройки". В раздел "Настройки" также можно попасть, нажав
+ЛКМ    на    кнопку    в    правом    верхнем    углу   главного    окна,
+отображающую текущий уровень сложности.
+        Для   каждого   уровня   сложности    устанавливается    свой
+коэффициент    для   вычисления    количества    мин   на    поле.
+Количество мин также зависит от размеров игрового поля. """)
         tb2.configure(state="disabled")
         tb2.grid(row=3, column=0, columnspan=10, padx=10, pady=10)
 
-        # frm_1 = CTkFrame(frm_0, fg_color="red", corner_radius=10, width=600, height=50)
-        # frm_1.pack()
-        # lbl2 = CTkLabel(frm_1, text=txt, width=280, height=50,
-        #                 corner_radius=10, bg_color="green")
-        # lbl2.pack(fill=tk.BOTH)
-        #
-        #
+        # заголовок "Управление"
+        CTkButton(frm_0, text="УПРАВЛЕНИЕ.", width=280, height=50, corner_radius=10,
+                         text_color="black", font=("Arial", 14,), hover=False,
+                         fg_color="transparent").grid(row=4, column=0, columnspan=10)
 
-        # lbl1.bind("<Button-1>", lambda event: help_win.title(help_win.geometry()))
-        print(frm_0.grid_size())
+        # способы управления с картинками
+        text_list = [
+            "ЛЕВАЯ КНОПКА МЫШИ:\nоткрыть клетку игрового поля",
+            "ПРАВАЯ КНОПКА МЫШИ:\nустановить/снять флажок на клетку",
+            "СРЕДНЯЯ КНОПКА МЫШИ:\nпоказать соседей для клетки",
+            "ПРАВАЯ + ЛЕВАЯ КНОПКИ МЫШИ:\nоткрыть неоткрытых соседей для клетки (!)"
+        ]
 
+        for i in range(4):
+            try:
+                h_img = CTkImage(light_image=Image.open(f"images/h0{i+2}.png"),
+                                    dark_image=Image.open(f"images/h0{i+2}.png"), size=(60, 60))
+                CTkLabel(frm_0, image=h_img, text="").grid(row=i+5, column=0, columnspan=2,
+                                                           padx=25, pady=5, sticky="w")
+            except Exception as msg:
+                print(msg)
+            tb = CTkTextbox(frm_0, width=300, height=60, fg_color="grey90", corner_radius=10, )
+            tb.insert("0.0", text_list[i])
+            tb.configure(state="disabled")
+            tb.grid(row=i+5, column=2, columnspan=7, padx=10, pady=10)
 
-
-
-        # print(type(help_win))
-        # help_win.bind_class("customtkinter.windows.ctk_toplevel.CTkToplevel", "<Leave>", lambda event: print("out"))
         help_win.grab_set()
         help_win.focus_set()
-
-        """
-                        
-        ПРАВИЛА ИГРЫ.
-            
-            Для победы в игре необходимо пометить флажками все мины на игровом поле. 
-        Чтобы начать игру, нужно левой клавишей мыши кликнуть в любую клетку на игровом поле.
-        Если в соседних клетках от первоначальной есть мины, то в клетке отображается их количество. 
-        <соседи>
-        """
-        """
-            Если в соседних клетках нет мин, то клетка останется пустой.
-        Если у клетки в соседях есть другие пустые клетки, а у тех, в свою очередь, свои,
-        то все такие клетки откроютсятся автоматически.
-        По похожему принципу происходит происходят открытия в остальных клетках на игровом поле.
-        Но с одним отличием: в любой из клеток может оказаться мина.
-        Если открыть клетку с миной, то произойдет взрыв и игра закончится.
-        Для успешного прохождения игры необходимо, ориентируясь на количество мин в открытых клетках,
-        пометить флажками все заминированые клетки.
-        """
-        """
-        НАСТРОЙКИ
-        """
-        """
-        УПРАВЛЕНИЕ
-        """
